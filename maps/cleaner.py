1a8e2fd300c25bf7da1f424086ddb07aec4ea434
Samizzo <samizzographics@gmail.com>
2019-11-01 21:59:57 -0700

    adding stuff

---
+++ maps/cleaner.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as pt
+
+
+dataset = pd.read_csv('extract_elevation.csv')
+
+pd.set_option('display.max_rows',2000)
+pd.set_option('display.max_columns',500)
+pd.set_option('display.width',1000)
+
+toDrop = [ 'UNIX TIME','TIME', 'DIST', 'HR', 'CAD', 'TEMP', 'POWER']
+
+dataset.drop(columns=toDrop, inplace=True, axis=1)
+
+#save csv
+df= pd.DataFrame(dataset)
+df.to_csv('cleaned.csv')
+print(dataset)
+
---
+++ maps/graph.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as pt
+
+#import csv data using pandas
+dataset = pd.read_csv('extract_elevation.csv')
+
+pd.Index()
