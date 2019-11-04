3ad5e4bf38b49272bb4f18399a88c938bd04f43c
Samizzo <samizzographics@gmail.com>
2019-11-04 08:59:12 +0300

    final aoutput

--- maps/cleaner.py
+++ maps/cleaner.py
@@ -3,18 +3,18 @@ import numpy as np
 from matplotlib import pyplot as pt
 
 
-dataset = pd.read_csv('extract_elevation.csv')
+dataset = pd.read_csv('farmer.csv')
 
 pd.set_option('display.max_rows',2000)
 pd.set_option('display.max_columns',500)
 pd.set_option('display.width',1000)
 
-toDrop = [ 'UNIX TIME','TIME', 'DIST', 'HR', 'CAD', 'TEMP', 'POWER']
+#toDrop = [ 'UNIX TIME','TIME', 'DIST', 'HR', 'CAD', 'TEMP', 'POWER']
 
-dataset.drop(columns=toDrop, inplace=True, axis=1)
+#dataset.drop(columns=toDrop, inplace=True, axis=1)
 
 #save csv
-df= pd.DataFrame(dataset)
-df.to_csv('cleaned.csv')
+#df= pd.DataFrame(dataset)
+#df.to_csv('cleaned.csv')
 print(dataset)
 
